864f1489384653beb04e42d563764d9ffe4ba183
srajangupta144 <srajangupta144@gmail.com>
2025-06-21 20:56:58 +0530

    name changes

--- springbootwebtutorial/src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/EmployeeDepartmentAPI.java
+++ springbootwebtutorial/src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/EmployeeDepartmentAPI.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootWebTutorialApplication {
+public class EmployeeDepartmentAPI {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebTutorialApplication.class, args);
+		SpringApplication.run(EmployeeDepartmentAPI.class, args);
 	}
 
 }
